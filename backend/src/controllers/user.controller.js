7845dd09af0e1326779c95112ae6d9d43b8dab51
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-03-12 03:40:46 +0530

    develop user profile view method

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -70,4 +70,21 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { loginUser, registerUser };
+const getProfile = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.user.id, {
+      attributes: ["id", "username", "email"], // Exclude password
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = { loginUser, registerUser, getProfile };
