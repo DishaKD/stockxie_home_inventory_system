2db81e7a2112ebe766b1a118fce3ecd6b0362230
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-03-15 15:53:42 +0530

    created product model

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,42 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config/db").sequelize; // Import the Sequelize instance
+
+const Product = sequelize.define("Product", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.literal(
+      "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+    ),
+  },
+});
+
+module.exports = Product;
