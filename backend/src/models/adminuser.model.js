03a13bdce810cc465a4f7eed51d1be3ab1d7f7f2
AnjaliMoragaspitiya <159754686+AnjaliMoragaspitiya@users.noreply.github.com>
2025-03-22 14:37:30 +0530

    created admin user migration and model

---
+++ backend/src/models/adminuser.model.js
@@ -0,0 +1,40 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config/db").sequelize;
+
+const AdminUser = sequelize.define(
+  "AdminUser",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "admin",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = AdminUser;
