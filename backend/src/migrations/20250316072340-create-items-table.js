4af511cc9eef9a43bc350bb7ee186df823d36a65
DishaKD <dasanayakedishal@gmail.com>
2025-03-23 00:27:31 +0530

    chnage drop sections

--- backend/src/migrations/20250316072340-create-items-table.js
+++ backend/src/migrations/20250316072340-create-items-table.js
@@ -42,6 +42,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Items");
+    await queryInterface.dropTable("Item.models");
   },
 };
--- backend/src/migrations/20250322093820-create-purchase-history.js
+++ backend/src/migrations/20250322093820-create-purchase-history.js
@@ -55,6 +55,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("PurchaseHistories");
+    await queryInterface.dropTable("PurchaseHistory.models");
   },
 };
