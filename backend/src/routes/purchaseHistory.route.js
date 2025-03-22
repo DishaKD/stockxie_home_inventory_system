06d124f1f1c0c5cfdc59227a34f8de9146b0d1d2
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-03-22 21:32:47 +0530

    fix route errors purchase history

--- backend/src/routes/purchaseHistory.route.js
+++ backend/src/routes/purchaseHistory.route.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const purchaseHistoryController = require('../controllers/purchaseHistory.controller');
+const express = require("express");
+const purchaseHistoryController = require("../controllers/purchaseHistory.controller");
+const purchaseRoutes = express.Router();
 
-router.post('/', purchaseHistoryController.createPurchase);  // Create a new purchase record
-router.get('/', purchaseHistoryController.getAllPurchases);  // Get all purchase records
-router.get('/:id', purchaseHistoryController.getPurchaseById);  // Get a single purchase by ID
-router.put('/:id', purchaseHistoryController.updatePurchase);  // Update a purchase record
-router.delete('/:id', purchaseHistoryController.deletePurchase);  // Delete a purchase record
+purchaseRoutes.post("/", purchaseHistoryController.createPurchase); // Create a new purchase record
+purchaseRoutes.get("/", purchaseHistoryController.getAllPurchases); // Get all purchase records
+purchaseRoutes.get("/:id", purchaseHistoryController.getPurchaseById); // Get a single purchase by ID
+purchaseRoutes.put("/:id", purchaseHistoryController.updatePurchase); // Update a purchase record
+purchaseRoutes.delete("/:id", purchaseHistoryController.deletePurchase); // Delete a purchase record
 
-module.exports = router;
+module.exports = purchaseRoutes;
