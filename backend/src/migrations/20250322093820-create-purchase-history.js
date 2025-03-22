7b9f1995045e1510a0ed2af39ed5dadbde1e9113
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-03-22 21:32:30 +0530

    fix migration file

--- backend/src/migrations/20250322093820-create-purchase-history.js
+++ backend/src/migrations/20250322093820-create-purchase-history.js
@@ -1,22 +1,60 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("PurchaseHistories", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      itemName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      purchaseDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+        },
+      },
+      pricePerUnit: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        validate: {
+          min: 0.01,
+        },
+      },
+      totalCost: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        validate: {
+          min: 0.01,
+        },
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("PurchaseHistories");
+  },
 };
