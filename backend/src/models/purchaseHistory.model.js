2a505f7aa06a19fe798110493efff7f394c6bb5f
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-03-22 21:42:43 +0530

    fix imports errors

--- backend/src/models/purchaseHistory.model.js
+++ backend/src/models/purchaseHistory.model.js
@@ -1,47 +1,51 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
 
-const PurchaseHistory = sequelize.define('PurchaseHistory', {
+const PurchaseHistory = sequelize.define(
+  "PurchaseHistory",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     itemName: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     category: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     purchaseDate: {
-        type: DataTypes.DATE,
-        allowNull: false
+      type: DataTypes.DATE,
+      allowNull: false,
     },
     quantity: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-            min: 1
-        }
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+      },
     },
     pricePerUnit: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-        validate: {
-            min: 0.01
-        }
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: 0.01,
+      },
     },
     totalCost: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-        validate: {
-            min: 0.01
-        }
-    }
-}, {
-    timestamps: true
-});
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: 0.01,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = PurchaseHistory;
