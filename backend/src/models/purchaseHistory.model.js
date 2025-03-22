a59f5126cf611c3d54f4b68267045d8379298a68
it22319838 <it22319838@my.sliit.lk>
2025-03-22 21:19:39 +0530

    addmodel

---
+++ backend/src/models/purchaseHistory.model.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const PurchaseHistory = sequelize.define('PurchaseHistory', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    itemName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    category: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    purchaseDate: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            min: 1
+        }
+    },
+    pricePerUnit: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            min: 0.01
+        }
+    },
+    totalCost: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            min: 0.01
+        }
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = PurchaseHistory;
