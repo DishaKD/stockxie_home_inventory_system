d0a6d3cc1249b2f1e41bc3ab37f71b38e6e38052
DishaKD <dasanayakedishal@gmail.com>
2025-03-23 01:28:13 +0530

    create api for create items

---
+++ Mobile Application/Stockxie-mobile-app/config/api/itemCreate.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { BASE_URL, ENDPOINTS } from "../index";
+
+// Function to create an item
+export const createItem = async (data: {
+  name: string;
+  quantity: number;
+  category: string;
+  expiryDate: string;
+}) => {
+  try {
+    const response = await axios.post(
+      `${BASE_URL}${ENDPOINTS.post.items}`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error creating item:", error);
+    throw error;
+  }
+};
