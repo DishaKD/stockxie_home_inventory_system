af42154b1aa5ddce55934675a22c12956e5472db
AnjaliMoragaspitiya <159754686+AnjaliMoragaspitiya@users.noreply.github.com>
2025-03-22 14:38:07 +0530

    admin route develop

---
+++ backend/src/routes/adminuser.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const adminUserController = require("../controllers/adminuser.controller");
+
+const adminUserRoutes = express.Router();
+
+// Routes
+adminUserRoutes.post("/", adminUserController.createAdminUser);
+adminUserRoutes.get("/", adminUserController.getAllAdminUsers);
+adminUserRoutes.get("/:id", adminUserController.getAdminUserById);
+adminUserRoutes.put("/:id", adminUserController.updateAdminUser);
+adminUserRoutes.delete("/:id", adminUserController.deleteAdminUser);
+
+module.exports = adminUserRoutes;
