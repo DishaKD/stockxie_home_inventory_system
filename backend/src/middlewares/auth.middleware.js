793ae791477b92b51276808d46a76cdecb6a5fe9
DishaKD <dasanayakedishal@gmail.com>
2025-03-25 04:25:28 +0530

    update authmiddleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -3,6 +3,12 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
+  if (!process.env.JWT_SECRET) {
+    return res
+      .status(500)
+      .json({ message: "JWT secret is not set in environment variables" });
+  }
+
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
   }
@@ -11,10 +17,21 @@ const authMiddleware = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify token
+
     req.user = decoded; // Attach user info to request object
     next(); // Proceed to the next middleware
   } catch (error) {
-    return res.status(403).json({ message: "Forbidden: Invalid token" });
+    if (error.name === "TokenExpiredError") {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: Token has expired" });
+    } else if (error.name === "JsonWebTokenError") {
+      return res.status(403).json({ message: "Forbidden: Invalid token" });
+    } else {
+      return res
+        .status(500)
+        .json({ message: "Server Error: Failed to authenticate token" });
+    }
   }
 };
 
