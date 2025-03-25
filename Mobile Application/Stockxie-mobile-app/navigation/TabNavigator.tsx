cec5cbbcd1ae3d9ac3ad9524300892dc49d7d8ab
DishaKD <dasanayakedishal@gmail.com>
2025-03-25 06:57:05 +0530

    refactor TabNavigator to use token in Purchase screen and improve type definitions

--- Mobile Application/Stockxie-mobile-app/navigation/TabNavigator.tsx
+++ Mobile Application/Stockxie-mobile-app/navigation/TabNavigator.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { screens } from "../app/screens";
 import { useAppSelector } from "../hooks";
+import type { RootStackParamList } from "../types";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-interface TabNavigatorProps {
-  route?: any;
-}
+type Props = NativeStackScreenProps<RootStackParamList, "TabNavigator">;
 
-const TabNavigator: React.FC<TabNavigatorProps> = ({ route }): JSX.Element => {
+const TabNavigator: React.FC<Props> = ({ route }): JSX.Element => {
   const userId = route?.params?.userId;
-  console.log("User ID in TabNavigator:", userId);
+  const token = route?.params?.token;
+  console.log("Token received:", token);
+
   const currentTabScreen = useAppSelector((state) => state.tab.screen);
 
   const renderScreen = () => {
     return (
       <React.Fragment>
         {currentTabScreen === "Home" && <screens.Home />}
-        {currentTabScreen === "Purchase" && <screens.Order />}
+        {currentTabScreen === "Purchase" && <screens.Purchase token={token} />}
         {currentTabScreen === "Notification" && <screens.Notification />}
         {currentTabScreen === "Inventory" && (
           <screens.Inventory userId={userId} />
