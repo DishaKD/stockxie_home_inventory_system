dff14a2f0e5c3e16b7f065cff00f6c2a8ffe77fd
DishaKD <dasanayakedishal@gmail.com>
2025-04-01 02:13:28 +0530

    add AuthenticateRoute component for protected route access

---
+++ Admin Panel/src/router/AuthenticateRoute.tsx
@@ -0,0 +1,21 @@
+// src/routes/AuthenticateRoute.tsx
+import { useSelector } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
+import { IRootState } from '../store'; // Import from your updated root reducer
+
+interface AuthenticateRouteProps {
+    element: JSX.Element;
+}
+
+const AuthenticateRoute: React.FC<AuthenticateRouteProps> = ({ element }) => {
+    const location = useLocation();
+    const { isAuthenticated } = useSelector((state: IRootState) => state.auth); // Now properly typed
+
+    if (!isAuthenticated) {
+        return <Navigate to="/auth/cover-login" state={{ from: location }} replace />;
+    }
+
+    return element;
+};
+
+export default AuthenticateRoute;
