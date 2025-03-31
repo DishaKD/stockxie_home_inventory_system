1262b303d415bf8c60acc84013a18502ed4e539e
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-04-01 03:23:52 +0530

    update sidebar navigation to link to user management page

--- Admin Panel/src/components/Layouts/Sidebar.tsx
+++ Admin Panel/src/components/Layouts/Sidebar.tsx
@@ -129,7 +129,7 @@ const Sidebar = () => {
                                         </NavLink>
                                     </li>
                                     <li className="nav-item">
-                                        <NavLink to="/apps/mailbox" className="group">
+                                        <NavLink to="/user-management/user-management" className="group">
                                             <div className="flex items-center">
                                                 <IconMenuUserManagement className="group-hover:!text-[#04b4bc] shrink-0" />
                                                 <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Users Management')}</span>
