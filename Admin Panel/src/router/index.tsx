bff19b029cd46a3e6cc66a2f825bb623b1ca2bc7
DishaKD <dasanayakedishal@gmail.com>
2025-04-01 02:13:54 +0530

    add authentication wrapper for protected routes in router configuration

--- Admin Panel/src/router/index.tsx
+++ Admin Panel/src/router/index.tsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import BlankLayout from '../components/Layouts/BlankLayout';
 import DefaultLayout from '../components/Layouts/DefaultLayout';
+import AuthenticateRoute from './AuthenticateRoute'; // Import your auth wrapper
 import { routes } from './routes';
 
 const finalRoutes = routes.map((route) => {
     return {
         ...route,
-        element: route.layout === 'blank' ? <BlankLayout>{route.element}</BlankLayout> : <DefaultLayout>{route.element}</DefaultLayout>,
+        element:
+            route.layout === 'blank' ? (
+                <BlankLayout>{route.protected ? <AuthenticateRoute element={route.element} /> : route.element}</BlankLayout>
+            ) : (
+                <DefaultLayout>{route.protected ? <AuthenticateRoute element={route.element} /> : route.element}</DefaultLayout>
+            ),
     };
 });
 
