3c06090e324aa06eeed4efbdba9a105d539a7ede
Prabodha <dinushipathiraja1@gmail.com>
2025-04-26 12:25:31 +0530

    edit user routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -3,6 +3,7 @@ const {
   loginUser,
   registerUser,
   getProfile,
+  getAllUsers,
 } = require("../controllers/user.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 
@@ -12,5 +13,8 @@ authRoutes.post("/login", loginUser);
 authRoutes.post("/register", registerUser);
 
 authRoutes.get("/profile", authMiddleware, getProfile);
+
+authRoutes.get("/users",getAllUsers); // or just authenticate
+
 
 module.exports = authRoutes;
