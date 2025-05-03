6efd4335371763c33bf17ccd78f51ce6353cf32d
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-05-04 03:28:45 +0530

    feat: create migration for Budgets table with userId, totalBudget, and totalSpent fields

---
+++ backend/src/migrations/20250503205925-create-budget-tracker.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Budgets", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "Users", key: "id" },
+        onDelete: "CASCADE",
+      },
+      totalBudget: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      totalSpent: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0.0,
+      },
+      month: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("Budgets");
+  },
+};
